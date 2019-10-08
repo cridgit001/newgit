37b709fe4742d531accad9bf605db33a0c95f8f0
Joshua_Rhodes <jdw.rhodes@gmail.com>
2019-10-08 14:12:35 -0500

    "New Practice code"

---
+++ practice.py
@@ -0,0 +1,8 @@
+user_input = str.lower(input("Who's better? Roger or Bugs?"))
+
+if user_input == 'roger':
+    print('Shave and a haircut')
+elif user_input == 'bugs':
+    print("What's up doc?")
+else:
+    print('Run away! Run away!')
