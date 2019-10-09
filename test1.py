634efd69c3fbcd3c51dbc336ab41be7d9b900a04
Joshua_Rhodes <jdw.rhodes@gmail.com>
2019-10-09 15:01:48 -0500

    Number guessing game

--- test1.py
+++ test1.py
@@ -1,1 +1,14 @@
-print("Good bye world")
+guess_counter = 3
+
+secret_number = 3
+
+while guess_counter != 0:
+    guess = int(input('Guess a number between 0-10.'))
+    guess_counter -= 1
+    if guess == secret_number:
+        print("That's right!")
+        break
+    else:
+        print("Sorry, try again.")
+else:
+    print('You lose.')
