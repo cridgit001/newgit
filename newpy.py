ee1f3d7f44fc7d215c36d91dfec23f6a105d3e29
Joshua_Rhodes <jdw.rhodes@gmail.com>
2019-10-11 10:43:00 -0500

    Practice changes

--- newpy.py
+++ newpy.py
@@ -1,1 +1,12 @@
-print('HelloWorld')
+#print('HelloWorld')
+
+y = int(input('Enter Number'))
+
+x = y // 2
+while x > 1:
+    if y % x == 0:
+        print(y, 'has factor', x)
+        break
+    x -= 1
+else:
+    print(y, 'is prime')
