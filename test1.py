0b2171e5f94ad4de08fc47cd0fd974d266122550
Joshua_Rhodes <jdw.rhodes@gmail.com>
2019-10-16 15:10:41 -0500

    Added more practice scripts

--- test1.py
+++ test1.py
@@ -1,14 +1,18 @@
-guess_counter = 3
+#guess_counter = 3
 
-secret_number = 3
+#secret_number = 3
 
-while guess_counter:
-    guess = int(input('Guess a number between 0-10.'))
-    guess_counter -= 1
-    if guess == secret_number:
-        print("That's right!")
-        break
-    else:
-        print("Sorry, try again.")
-else:
-    print('You lose.')
+#while guess_counter:
+#    guess = int(input('Guess a number between 0-10.'))
+#    guess_counter -= 1
+#    if guess == secret_number:
+#        print("That's right!")
+#        break
+#    else:
+#        print("Sorry, try again.")
+#else:
+#    print('You lose.')
+
+x = range(1,10)
+
+print(x)
