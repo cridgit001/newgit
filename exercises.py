55c85cf89259833cde1175ab1f1c9596e68d3d18
Joshua_Rhodes <jdw.rhodes@gmail.com>
2019-11-05 13:57:47 -0600

    Chapter 15 Exercises

---
+++ exercises.py
@@ -0,0 +1,25 @@
+L = [1,2,4,8,16,32,64]
+X = 5
+i = 0
+
+
+
+#found = False
+
+#while i < len(L):
+#    if 2 ** X == L[i]:
+#        print('at index', i)
+#        break
+#    else:
+#        i = i + 1
+#else:
+#    print(X, 'not found')
+#if found:
+#    print('at index', i)
+#else:
+#    print(X, 'not found')
+
+#for x in L:
+#    if x == 2 ** X:
+#        print('at index', L.index(x))
+#        break
